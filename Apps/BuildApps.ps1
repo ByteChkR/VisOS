0aa13e29ad43c5fc845d2c59903bd468083bfe9f
Tim Akermann <ti.akermann@gmail.com>
2021-01-13 03:42:10 +0100

    Addded Default Build Config Files Applied shorter commandline arguments

--- Apps/BuildApps.ps1
+++ Apps/BuildApps.ps1
@@ -1,39 +1,23 @@
 
 
 cd VisOS.BaseCommands
-vis project clean
-vis project pack
 vis project publish $UPLOAD_REPO
-vis project clean
 vis project restore $DOWNLOAD_REPO
-
-vis build -build:i App.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 8192 -linker:export
+vis build -cli:configs default.build.args
 
 cd ..\VisOS.UnitTests
-vis project clean
-vis project pack
 vis project publish $UPLOAD_REPO
-vis project clean
 vis project restore $DOWNLOAD_REPO
-
-vis build -build:i UnitTests.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 32768 -linker:export
+vis build -cli:configs default.build.args
 
 cd ..\VisOS.HFSCommands
-vis project clean
-vis project pack
 vis project publish $UPLOAD_REPO
-vis project clean
 vis project restore $DOWNLOAD_REPO
-
-vis build -build:i App.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 4096 -linker:export
+vis build -cli:configs default.build.args
 
 cd ..\VisOS.Benchmarks
-vis project clean
-vis project pack
 vis project publish $UPLOAD_REPO
-vis project clean
 vis project restore $DOWNLOAD_REPO
-
-vis build -build:i RunBenchmarks.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 0 -linker:export
+vis build -cli:configs default.build.args
 
 cd ..
--- Build.sh
+++ Build.sh
@@ -4,24 +4,17 @@ UPLOAD_REPO="local"
 DOWNLOAD_REPO="local"
 
 cd VisOS.Driver
-$vis project clean
-$vis project pack
 $vis project publish $UPLOAD_REPO
-$vis project clean
 
 cd ../BootLoader
-$vis project clean
-$vis project pack
 $vis project publish $UPLOAD_REPO
 $vis project restore
-$vis build -build:i Loader.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 0 -linker:export
+$vis build -cli:configs default.build.args
 
 cd ../VisOS
-$vis project clean
-$vis project pack
 $vis project publish $UPLOAD_REPO
 $vis project restore
-$vis build -build:i Core.vhl -build:steps HL-expr bin -build:clean false -assembler:offset.global 16384 -linker:export
+$vis build -cli:configs default.build.args
 cd ..
 
 cd Apps
